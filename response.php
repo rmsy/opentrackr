b617b7c1f206e4544ac40fe367ff3b4ae602efe8
Terence Tan <terencetanwj@gmail.com>
2012-06-04 16:34:56 +0800

    Initial commit

---
+++ response.php
@@ -0,0 +1,23 @@
+<?php
+global $config;
+$key = openssl_get_publickey($config['cert']);
+class Response {
+
+    public $data = array();
+    public $code;
+
+    public function send($signature = null) {
+        $json = array(
+            'code' => $this->code,
+            'data' => $this->data
+        );
+        if ($signature == true) {
+            global $key;
+            openssl_sign($this->data, $sig, $key);
+            $json['signature'] = base64_encode($sig);
+        }
+        echo json_encode($json);
+        die(0);
+
+    }
+}
