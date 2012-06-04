d04a46d13c887aa188f1ced714488530c28af652
Terence Tan <terencetanwj@gmail.com>
2012-06-04 23:16:31 +0800

    more stuff

---
+++ opentrackr.config.php
@@ -0,0 +1,9 @@
+<?php
+/* opentrackr config *
+*/
+
+/* Database Configuration */
+$config['db']['url'] = "http://localhost:5984" //CouchDB address
+$config['db']['name'] = "opentrackr" //Database
+
+//That's about it.. for now
---
+++ opentrackr/lib/db/Bean.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: ttwj
+ * Date: 4/6/12
+ * Time: 9:21 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Bean
+{
+
+}
